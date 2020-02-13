1e548d688c5c42978f2c93018ed6949f1e605850
niehen6174 <42087078+niehen6174@users.noreply.github.com>
2020-02-13 13:39:19 +0800

    Add files via upload

---
+++ FRID智能控制门禁/FRID_arduino/FRID_arduino.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 10
+#define RST_PIN 9
+int mSwitch = 2 ; int LEDRed = 3 ; int LEDGreen = 4 ;//设定电磁阀开关及红绿灯Pin值
+MFRC522 mfrc522 ( SS_PIN , RST_PIN ); //建立MFRC522.<br />
+void setup () {
+pinMode ( LEDRed , OUTPUT );
+pinMode ( LEDGreen , OUTPUT );
+pinMode ( mSwitch , OUTPUT );
+Serial . begin ( 9600 ); //设定串列通讯速率
+SPI . begin (); //设定SPI bus
+mfrc522 . PCD_Init (); //设定MFRC522
+Serial . println ( " Put your card close to the reader… " );
+Serial . println ();
+}
+void loop () {
+digitalWrite ( LEDGreen , LOW );
+digitalWrite ( LEDRed , LOW );
+digitalWrite ( mSwitch , LOW );
+if ( ! mfrc522 . PICC_IsNewCardPresent ()) { return ; } //
+if ( ! mfrc522 . PICC_ReadCardSerial ()) { return ; } //Show UID on serial monitor
+Serial.print ( "Card No. :"
+) ;
+String content = "" ;
+byte letter ;
+for ( byte i = 0 ; i < mfrc522 . uid . size ; i ++)
+{
+Serial . print ( mfrc522 . uid . uidByte [ i ] < 0x10 ? " 0" : " " );
+Serial . print ( mfrc522 . uid . uidByte [ i ], HEX );
+content . concat ( String ( mfrc522 . uid . uidByte [ i ] < 0x10 ? " 0" : " " ));
+content . concat ( String ( mfrc522 . uid . uidByte [ i ], HEX ));
+}
+Serial . println ();
+Serial . print ( " Message : " );
+content . toUpperCase ();<br />
+//需要改成可通行的卡号
+if ( content . substring ( 1 ) == "FA 91 83 08" ) {
+Serial . println ( " Pass… " );
+Serial . println (); //亮绿灯,电磁阀开
+digitalWrite ( LEDGreen , HIGH );
+digitalWrite ( LEDRed , LOW );
+digitalWrite ( mSwitch , HIGH );
+delay ( 3000 );
+}
+else { Serial . println ( " Stop…" ); //亮红灯,电磁阀关
+digitalWrite ( LEDGreen , LOW );
+digitalWrite ( LEDRed , HIGH );
+digitalWrite ( mSwitch , LOW );
+delay ( 3000 );
+}
+}
---
+++ FRID智能控制门禁/read_code/read_code.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <MFRC522.h>     // 引用程式庫
+
+#define RST_PIN      A0        // 讀卡機的重置腳位
+#define SS_PIN       10        // 晶片選擇腳位
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // 建立MFRC522物件
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("RFID reader is ready!");
+
+  SPI.begin();
+  mfrc522.PCD_Init();   // 初始化MFRC522讀卡機模組
+}
+
+void loop() {
+    // 確認是否有新卡片
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+      byte *id = mfrc522.uid.uidByte;   // 取得卡片的UID
+      byte idSize = mfrc522.uid.size;   // 取得UID的長度
+
+      Serial.print("PICC type: ");      // 顯示卡片類型
+      // 根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
+      MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+      Serial.println(mfrc522.PICC_GetTypeName(piccType));
+
+      Serial.print("UID Size: ");       // 顯示卡片的UID長度值
+      Serial.println(idSize);
+ 
+      for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
+        Serial.print("id[");
+        Serial.print(i);
+        Serial.print("]: ");
+        Serial.println(id[i], HEX);       // 以16進位顯示UID值
+      }
+      Serial.println();
+
+      mfrc522.PICC_HaltA();  // 卡片停止模式
+    } 
+}
